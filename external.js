5c340d18b1a5349a14dacd00a044ca94aacf652d
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-01-25 14:59:53 -0600

    plan and add pseudocode for initial first steps, link external.js

---
+++ external.js
@@ -0,0 +1,4 @@
+//user vs computer rock-paper-scissors-begin with a function called getComputerChoice,
+//this function will randomly choose between rock, paper, or scissors.
+//Tip use console to ensure this is returning as expected before moving on.
+//Write a function that plays single round, function should take two paramters(playerSelection, computerSelection)
