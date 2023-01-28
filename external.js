446beb40e52c2d572af2693a76f98836dd96f6b5
Jonathan Kennedy <gt4magnet@protonmail.com>
2023-01-27 23:29:52 -0600

    redo with own code only, no stack overflow help

--- external.js
+++ external.js
@@ -1,64 +1,71 @@
+//user vs computer rock-paper-scissors-begin with a function called getComputerChoice,
+//this function will randomly choose between rock, paper, or scissors.
+//Tip use console to ensure this is returning as expected before moving on.
+//Works correctly so far.
 
-// Determines computer choice and randomizes selection
 function getComputerChoice() {
-    const options = ["Rock", "Paper", "Scissors"];
-    const randomChoice = Math.floor(Math.random() * options.length);
-    return options[randomChoice];
-}
-
-//Runs the game
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let theWinner = "";
-
-    // loop game for 5 rounds. Track rounds and score
-    for (i = 0; i < 5; i++) {
-        const computerSelection = getComputerChoice().toLowerCase();
-        const playerSelection = prompt("Choose Your Weapon", "Rock, Paper, Scissors").toLowerCase();
-        // console.log(playerSelection)
-        // console.log(computerSelection)
-        console.log(playRound(playerSelection, computerSelection));
-        console.log("Player Win Totals " + playerScore);
-        console.log("Computer Win Totals " + computerScore);
-
-        // Plays round to determine winner
-        function playRound(playerSelection, computerSelection) {
-            if (playerSelection === computerSelection) {
-                return alert(`It's a Tie, You both picked ${playerSelection}`);
-            } else if (playerSelection === "rock" && computerSelection === "paper") {
-                computerScore++
-                return alert("The Computer Wins! Paper beats Rock");
-            } else if (playerSelection === "paper" && computerSelection === "scissors") {
-                computerScore++
-                return alert("The Computer Wins! Scissors beats Paper");
-            } else if (playerSelection === "scissors" && computerSelection === "rock") {
-                computerScore++
-                return alert("The Computer Wins! Rock beats Scissors");
-            } else if (playerSelection === "rock" && computerSelection === "scissors") {
-                playerScore++
-                return alert("Nice! You Win! Rock beats Scissors");
-            } else if (playerSelection === "paper" && computerSelection === "rock") {
-                playerScore++
-                return alert("Nice! You Win! Paper beats Rock");
-            } else if (playerSelection === "scissors" && computerSelection === "paper") {
-                playerScore++
-                return alert("Nice! You Win! Scissors beats Paper");
-            }
-        }
-    }
-    // determine winner of 5 round game  
-    if (playerScore > computerScore) {
-        theWinner = "Winner! You beat the Computer!";
-    } else if (playerScore === computerScore) {
-        theWinner = "You tied with the Computer. Try Again!"
+    const computerSelection = Math.floor(Math.random() * 3);
+    if (computerSelection === 0) {
+        return "rock";
+    } else if (computerSelection === 1) {
+        return "paper";
     } else {
-        theWinner = "Loser! You lost to the Computer"
-    }
-
-    // Reports who won the game  
-    alert("After Five Rounds, The Winner is: " + theWinner);
+        return "scissors";
+    };
 }
+//For easier code write a getPlayerChoice function, perhaps?
+//this function will randomly choose between rock, paper, or scissors.
+//correct console entries.
+function getPlayerChoice() {
+    var playerSelection = prompt("Rock, Paper, or Scissors?");
+    if (playerSelection.toLowerCase() === "rock") {
+        return "rock";
 
-// Starts the game
-game();
+    } else if (playerSelection.toLowerCase() === "paper") {
+        return "paper";
+
+    } else if (playerSelection.toLowerCase() === "scissors") {
+        return "scissors";
+
+    }
+};
+
+//Write a function that plays single round, function should take two paramters(playerSelection, computerSelection)
+//returns a string that declares the winner of the round, "You Lose! Paper beats Rock!" or "You Win! Rock beats Scissors!"
+//make sure playerSelection always returns lowercase letters to match function.
+//having issues with variables not comparing
+function playRound(playerSelection, computerSelection) {
+
+
+
+    if (playerSelection === computerSelection) {
+        return "Draw! Pick again!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors!", playerScore++;
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock!", computerScore++;
+
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock!", playerScore++;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lose! Scissors beats Paper!", computerScore++;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Scissors!", computerScore++;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper!", playerScore++;
+    }
+};
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
+//Write a function that plays multiple rounds Game(), 
+// Call playRound inside the function to play 5 rounds that keeps score and reports winner or loser at the end.
+
+function Game() {
+    //ensure prompt continues to run for 5 games.
+    // create game ranking for each round to sum up
+    playRound(getPlayerChoice(), getComputerChoice());
+    for (let i = 0; i <= 5; i++) {
+
+}};
+
+Game();
